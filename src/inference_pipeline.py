aeaddfa17b316de2fa069d1ef67bdf66f41d687c
Princewill Nwoko <81762660+manlikejiggy@users.noreply.github.com>
2024-06-01 13:47:50 +0100

    Update inference_pipeline.py

--- src/inference_pipeline.py
+++ src/inference_pipeline.py
@@ -10,7 +10,7 @@ from src.feature_store import get_feature_store
 import src.config as config 
 
 
-current_date = pd.to_datetime(datetime.now(), utc=True).floor('h')
+current_date = pd.to_datetime(datetime.now(), utc=True).floor('h')  + timedelta(hours=1)
 print(current_date)
 
 # Load Rides & Weather Data From Hopsworks Feature Store
